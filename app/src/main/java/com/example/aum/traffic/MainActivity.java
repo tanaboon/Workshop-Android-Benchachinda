1741f6b1f1e941d5e2182c63632ef2fc73fd4427
Doraemon <tanaboon.c@gmail.com>
2015-12-22 10:25:57 +0700

    ออกแบบ UI เสร็จแล้ว#3 Media

--- app/src/main/java/com/example/aum/traffic/MainActivity.java
+++ app/src/main/java/com/example/aum/traffic/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.aum.traffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -17,10 +21,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //Bind Widget
         bindWidget();
 
+        // button Controller
+
+        buttonController();
+
     } // Main Method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.lion);
+                buttonMediaPlayer.start();
+
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/rdvEBrFAnfs"));
+                startActivity(objIntent);
+            }       //event
+        });
+    }
 
     private void bindWidget() {
         trafficListView = (ListView) findViewById(R.id.listView);
